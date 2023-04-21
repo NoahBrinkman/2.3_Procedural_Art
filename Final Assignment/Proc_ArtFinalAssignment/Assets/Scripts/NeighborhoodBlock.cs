81f84741187e71aee75402ca9d04a28cfb181b72
Noah Brinkman <noahpbrinkman@gmail.com>
2023-04-21 16:55:56 +0200

    Final Assignment Start

---
+++ Final Assignment/Proc_ArtFinalAssignment/Assets/Scripts/NeighborhoodBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+public class NeighborhoodBlock : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public float cubeSize = 1f;
+
+    Vector2Int[] shapePoints;
+
+    public void SetShape(Vector2Int[] points)
+    {
+        shapePoints = points;
+
+        // Instantiate cubes at the shape points
+        foreach (Vector2Int point in shapePoints)
+        {
+            Vector3 position = new Vector3(point.x * cubeSize, 0f, point.y * cubeSize);
+            Instantiate(cubePrefab, position, Quaternion.identity, transform);
+        }
+    }
+}
